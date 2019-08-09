8e3890b5d999825096f4bb374282572db40facb3
Annacher1998 <chernyshova.anna1998@gmail.com>
2019-08-08 21:59:43 -0400

    addind extend logger to the test

--- src/test/java/com/BriteERP/tests/LoginDemo.java
+++ src/test/java/com/BriteERP/tests/LoginDemo.java
@@ -15,7 +15,7 @@ public class LoginDemo extends TestBase {
     @Test
     public void test1()  {
 
-
+        extentLogger = report.createTest("Verify that second opportunity’ should be the same in different views  ");
         String email = ConfigurationReader.get("eventManagerUsername");
         String password = ConfigurationReader.get("eventManagerPassword");
         LoginPage loginPage = new LoginPage();
@@ -39,19 +39,25 @@ public class LoginDemo extends TestBase {
 
         BrowserUtils.waitFor(4);
         crmPages.pivot.click();
+
         BrowserUtils.clickWithTimeOut(crmPages.minusTotal,1);
         BrowserUtils.clickWithTimeOut(crmPages.plusTotal,1);
         BrowserUtils.clickWithTimeOut(crmPages.opportunity,1);
         String getSecondRevenuePivot=crmPages.secondExpectedRevenuePivot.getText();
+        extentLogger.info("Verifying Second expected revenue on the pivot view");
         BrowserUtils.clickWithTimeOut(crmPages.list,3);
         String getSecondRevenueList=crmPages.secondExpectedRevenueList.getText();
+        extentLogger.info("Verifying Second expected revenue on the list view");
         Assert.assertEquals(getSecondRevenuePivot,getSecondRevenueList);
-crmPages.pivot.click();
-BRPUtils.getSum();
-String totalRevenue=Driver.get().findElement(By.xpath("//table[contains(@class,'table-hover table-condensed table')]/tbody/tr[1]/td[2]")).getText();
-totalRevenue=totalRevenue.replace(",","");
-double totalRevenue1=Double.parseDouble(totalRevenue)+0.01;
-Assert.assertEquals(totalRevenue1, BRPUtils.getSum());
+        extentLogger.pass("PASSED: verified that second revenue same in different views");
+        crmPages.pivot.click();
+        BRPUtils.getSum();
+
+        String totalRevenue=Driver.get().findElement(By.xpath("//table[contains(@class,'table-hover table-condensed table')]/tbody/tr[1]/td[2]")).getText();
+        totalRevenue=totalRevenue.replace(",","");
+        double totalRevenue1=Double.parseDouble(totalRevenue)+0.01;
+        Assert.assertEquals(totalRevenue1, BRPUtils.getSum());
+        extentLogger.pass("PASSED: verified that total of revenues same");
 
     }
 }
